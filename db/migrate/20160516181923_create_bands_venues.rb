bb920ce4cb6d11b183b489bf772bac09b1a7d535
Prelang Builder <builder@prelang.com>
2016-05-16 18:19:23 +0000

    Created migration for HABTM table between 'Band' and 'Venue'

---
+++ db/migrate/20160516181923_create_bands_venues.rb
@@ -0,0 +1,8 @@
+class CreateBandsVenues < ActiveRecord::Migration
+  def change
+    create_table :bands_venues do |t|
+      t.belongs_to :band, index: true
+      t.belongs_to :venue, index: true
+    end
+  end
+end
